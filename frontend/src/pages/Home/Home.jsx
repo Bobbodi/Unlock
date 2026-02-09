af66d0269937cb78e856c7fba3fcdd71ae053578
= <mannysally000@gmail.com>
2026-02-10 00:31:13 +0800

    Add button to pages for navigation

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,28 @@
+import { useNavigate } from "react-router-dom"
+
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const goToLogin = () => {
+        navigate("/login");
+    }
+
+    const goToProfile = () => {
+        navigate("/profile");
+    }
+
+    const goToChat = () => {
+        navigate("/chat");
+    }
+
     return (
         <div>
             Home Page
+
+            <button onClick={goToLogin}>Go to Login</button>
+            <button onClick={goToProfile}>Go to Profile</button>
+            <button onClick={goToChat}>Go to Chat</button>
         </div>
     )
 }
