62a2e45941f9634a2f3dffe510874b4f147b7e82
yatong yang <yyt@yatongs-MacBook-Air.local>
2026-02-10 08:36:33 +0800

    Add Login Logout feature

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -4,3 +4,6 @@ export const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_ANON_KEY
 );
+
+   console.log('URL:', process.env.REACT_APP_SUPABASE_URL);
+   console.log('KEY:', process.env.REACT_APP_SUPABASE_ANON_KEY);
